a01eb07dccd1493e7fd462b5add31bdbc931fad4
worldcoolguy <miguelsantiagoupwork925@gmail.com>
2017-06-28 22:47:50 +0100

    installed normalize.css and skeleton.css

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,4 @@
 // app.js is the main JS file which you should define your Angular module
+(function() {
+  angular.module('lbpf', []);
+})();
---
+++ src/js/controllers/home.controller.js
@@ -0,0 +1,7 @@
+angular.module('lbpf')
+  .controller('HomeCtrl', HomeCtrl);
+
+HomeCtrl.$inject = [];
+function HomeCtrl() {
+  const vm = this;
+}
